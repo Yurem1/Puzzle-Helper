2ae3c6bcd6d8e0c1df12a3d2929e612e7f9113dc
Yurem1 <yuremtovar1@gmail.com>
2023-10-04 14:47:41 -0500

    Created Tools.kt It is made up of functions that contain functions from different classes within the application. aka utility

---
+++ app/src/main/java/gg/yurem/puzzlehelper/Tools.kt
@@ -0,0 +1,21 @@
+package gg.yurem.puzzlehelper
+
+import android.app.Activity
+
+internal class Tools {
+
+    /* A mixture of utility functions
+     using functions from different classes within the
+    application */
+    companion object {
+        fun takePicture(thisContext: Activity) {
+
+            if(Permissions.hasCameraPermissions(thisContext)) {
+                Camera.takePicture(thisContext)
+            }
+            else {
+                Permissions.requestCameraPermission(thisContext)
+            }
+        }
+    }
+}
