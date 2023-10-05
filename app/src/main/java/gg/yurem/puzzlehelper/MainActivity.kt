59571adf120d5a467c8b32aca4f8623c5cfa358c
Yurem1 <yuremtovar1@gmail.com>
2023-10-05 09:25:47 -0500

    Added comments

--- app/src/main/java/gg/yurem/puzzlehelper/MainActivity.kt
+++ app/src/main/java/gg/yurem/puzzlehelper/MainActivity.kt
@@ -3,11 +3,12 @@ package gg.yurem.puzzlehelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/* No use for this class so far, however, it will stay intact for future use */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
     }
 }
